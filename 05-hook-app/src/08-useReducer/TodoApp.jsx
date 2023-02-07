c3a2b37e0719c1e8d6c3db5ea790f20b7d49d712
Cristopher-Quiros <cristopherquiros2@gmail.com>
2023-02-07 11:27:04 -0600

    commit

---
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,33 @@
+import { useReducer } from "react"
+import { todoReducer } from "./todoReducer"
+
+
+
+export const TodoApp = () => {
+
+    const initialState = [{
+        id: new Date().getTime(),
+        description: 'Recolectar la piedra del alma',
+        done: false
+    },
+    {
+        id: new Date().getTime() + 100,
+        description: 'Recolectar la piedra del alma',
+        done: false
+    }]
+
+    const [ todos, dispatch ] = useReducer(todoReducer, initialState)
+
+  return (
+    <>
+    <h1>TodoApp</h1>
+    <hr/>
+
+    <ul>
+        <li> Item 1 </li>
+        <li> Item 2 </li>
+        <li> Item 3 </li>
+    </ul>
+    </>
+  )
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { TodoApp } from './08-useReducer/TodoApp'
 // import { CallbackHook } from './06-Memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre'
 // import { MemoHook } from './06-Memos/MemoHook'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-Memos/Memorize'
@@ -12,13 +13,13 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { HooksApp } from './HooksApp'
-import './index.css'
-import './08-useReducer/intro-reducer'
+// import './index.css'
+// import './08-useReducer/intro-reducer'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />
+    <TodoApp />
   // </React.StrictMode>,
 )
